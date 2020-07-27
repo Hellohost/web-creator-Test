c4d3b5f7fbe4e1fdbb6b1a41d769171ea4f5b127
Denis <you@example.com>
2020-07-27 14:35:49 +0300

    first commit

---
+++ build/precache-manifest.43adee8ff38d8cc908a375db893b4322.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27db97cc34698eb35f0373cd24e231d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4878815f4fbf9bc19d",
+    "url": "/static/css/2.f568ff98.chunk.css"
+  },
+  {
+    "revision": "e1824bce9d4de51e1287",
+    "url": "/static/css/main.e02122b4.chunk.css"
+  },
+  {
+    "revision": "cd4878815f4fbf9bc19d",
+    "url": "/static/js/2.96120ad7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.96120ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1824bce9d4de51e1287",
+    "url": "/static/js/main.763708bb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
